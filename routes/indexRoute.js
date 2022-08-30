9f3c91bcbd67978554c8ae122691e38daa60b55a
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2022-08-30 10:57:12 +0300

    Add /error controller for error control

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -4,12 +4,17 @@ const router = express.Router();
 
 const isLoggedIn = require('../middleware/isLoggedIn');
 
+const errorGetController = require('../controllers/index/error/get');
 const indexGetController = require('../controllers/index/index/get');
 
 router.get(
   '/',
     isLoggedIn,
     indexGetController
+);
+router.get(
+  '/error',
+    errorGetController
 );
 
 module.exports = router;
